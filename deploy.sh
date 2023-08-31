3d272cd7b39c62d36ad4bb7e78902d24f490d169
Jesse Cranney <jesse.cranney@anu.edu.au>
2023-08-31 11:46:44 +1000

    auto deploy modification

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env sh
 
-# 当发生错误时中止脚本
+# Abort the script when an error occurs
 set -e
 
-# 构建
+# install
+npm install
+
+# build
 npm run build
 
-# cd 到构建输出的目录下
+# cd into dist directory to tidy for deployment
 cd dist
 cp index.html 404.html
 echo "universetbd.org" >> CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-git branch -m gh-pages
-
-# 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:universeTBD/universeTBD.github.io.git gh-pages:gh-pages
 
 cd -
